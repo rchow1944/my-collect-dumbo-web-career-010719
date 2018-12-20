54ddcd57ba7c5ad8023a009fad97390b2afeeaff
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 23:58:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   result = []
   while i < collection.length do
-    result.push(yield collection[i])
+    result << yield collection[i]
     i += 1
   end
   result
